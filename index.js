fd649835ae100eda30cfbf3bd516e6ee009744eb
Cecilia Avery <cilsong@gmail.com>
2017-12-28 03:10:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ function destructivelyAppendKitten(name) {
   kittens.push(name)
   return kittens
 }
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+  return kittens
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+  return kittens
+}
