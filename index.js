80e7f6ef7c2cdf742f23cfdbfe6b7bf04defa89a
Cecilia Avery <cilsong@gmail.com>
2017-12-28 03:08:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const app = "I don't do much."
-function destructivelyAppendKitten(arr, name) {
-  arr.unshift(name)
-  return arr
+function destructivelyAppendKitten(name) {
+  kittens.unshift(name)
+  return kittens
 }
